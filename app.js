d507a3cc35676be676017aa508b3c2840b0b4d85
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-01-24 19:12:55 -0600

    Finished first bonus

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 class Casino {
-    constructor(name) {
+    constructor(name, isFakeCoin) {
         this.name = name;
         this.timesPlayed = 0;
+        this.isFakeCoin = isFakeCoin;
     }
     playGame(betAmount) {
-        if (Math.random() <= 0.5) {
-            console.log("Casino Wins!");
+        if (Math.random() <= 0.5 || this.isFakeCoin) {
+            console.log(this.name + " Wins!");
         } else {
             this.timesPlayed++;
             console.log(
@@ -28,14 +29,13 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
 
 // Extra BONUS TESTS
 /*
